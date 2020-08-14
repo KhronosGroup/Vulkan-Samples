16c166ef46d5384c35bfd8d3fb20383ae585b60f
Carlos Dominguez Caballero <cdomingu@qti.qualcomm.com>
2020-08-14 14:38:41 -0700

    Fix regression introduced by PR154
    On some platforms the last character is not a quotation mark but a carriage return. This was introducing a crash on the samples from the 'Performance' tab while trying to file.open the lighting.h header as shown below:
    Abort message: 'terminating with uncaught exception of type std::runtime_erro': Failed to open file: /storage/emulated/0/Android/data/com.khronos.vulkan_samples/files/shaders/lighting.h"

--- framework/core/shader_module.cpp
+++ framework/core/shader_module.cpp
@@ -42,9 +42,10 @@ inline std::vector<std::string> precompile_shader(const std::string &source)
 		{
 			// Include paths are relative to the base shader directory
 			std::string include_path = line.substr(10);
-			if (!include_path.empty() && include_path.back() == '"')
+			size_t last_delimiter = include_path.find("\"");
+			if (!include_path.empty() && last_delimiter != std::string::npos)
 			{
-				include_path.pop_back();
+				include_path = include_path.substr(0, last_delimiter);
 			}
 
 			auto include_file = precompile_shader(fs::read_shader(include_path));
