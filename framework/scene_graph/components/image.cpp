adc3cea1d5cc86df6f0a05215728b27ac5bdd118
Sascha Willems <webmaster@saschawillems.de>
2021-05-04 19:28:12 +0200

    Add support for ktx2 image extension

--- framework/scene_graph/components/image.cpp
+++ framework/scene_graph/components/image.cpp
@@ -265,6 +265,10 @@ std::unique_ptr<Image> Image::load(const std::string &name, const std::string &u
 	{
 		image = std::make_unique<Ktx>(name, data);
 	}
+	else if (extension == "ktx2")
+	{
+		image = std::make_unique<Ktx>(name, data);
+	}
 
 	return image;
 }
