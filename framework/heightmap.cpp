9561c55fcbc89d862d2b5265287c7b76cdf16d56
Sascha Willems <webmaster@saschawillems.de>
2021-05-24 18:32:35 +0200

    Update libktx to version 4.0 - Adding support for KTX2 and Basis Universal Texture compression (#272)
    * Updated KTX submodule to latest 4.x release
    
    * Updated third party CMakeLists for libktx 4.0
    
    Includes support for KTX1, KTX2 and Basis Universal
    
    * Add support for ktx2 image extension
    
    * Updated ktx loading class for libktx 4.0
    
    Get Vulkan format from libktx
    
    * Adapted texture samples to libktx 4.0 changes
    
    * Android fixes for libktx 4.0
    
    * Added required pre-processor define for basis universal transcoder
    
    * Copyright headers
    
    * Fixing framework dependencies
    
    * Copyright headers
    
    * Clang format
    
    * Use dllexport on Windows only

--- framework/heightmap.cpp
+++ framework/heightmap.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019, Sascha Willems
+/* Copyright (c) 2019-2021, Sascha Willems
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -18,7 +18,6 @@
 #include "heightmap.h"
 
 #include <cstring>
-#include <ktx.h>
 
 #include "common/error.h"
 
