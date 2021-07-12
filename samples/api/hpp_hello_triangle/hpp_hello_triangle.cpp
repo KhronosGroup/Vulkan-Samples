3e62a9f5a339e539e4833c0594e0a8376991286d
asuessenbach <asuessenbach@nvidia.com>
2021-07-12 17:23:20 +0200

    Add local versions of get_optimal_validation_layers and validate_layers.

--- samples/api/hpp_hello_triangle/hpp_hello_triangle.cpp
+++ samples/api/hpp_hello_triangle/hpp_hello_triangle.cpp
@@ -135,6 +135,67 @@ VkShaderStageFlagBits HPPHelloTriangle::find_shader_stage(const std::string &ext
 	throw std::runtime_error("No Vulkan shader stage found for the file extension name.");
 };
 
+bool validate_layers(const std::vector<const char *> &       required,
+                     const std::vector<vk::LayerProperties> &available)
+{
+	for (auto layer : required)
+	{
+		bool found = false;
+		for (auto &available_layer : available)
+		{
+			if (strcmp(available_layer.layerName, layer) == 0)
+			{
+				found = true;
+				break;
+			}
+		}
+
+		if (!found)
+		{
+			LOGE("Validation Layer {} not found", layer);
+			return false;
+		}
+	}
+
+	return true;
+}
+
+std::vector<const char *> get_optimal_validation_layers(const std::vector<vk::LayerProperties> &supported_instance_layers)
+{
+	std::vector<std::vector<const char *>> validation_layer_priority_list =
+	    {
+	        // The preferred validation layer is "VK_LAYER_KHRONOS_validation"
+	        {"VK_LAYER_KHRONOS_validation"},
+
+	        // Otherwise we fallback to using the LunarG meta layer
+	        {"VK_LAYER_LUNARG_standard_validation"},
+
+	        // Otherwise we attempt to enable the individual layers that compose the LunarG meta layer since it doesn't exist
+	        {
+	            "VK_LAYER_GOOGLE_threading",
+	            "VK_LAYER_LUNARG_parameter_validation",
+	            "VK_LAYER_LUNARG_object_tracker",
+	            "VK_LAYER_LUNARG_core_validation",
+	            "VK_LAYER_GOOGLE_unique_objects",
+	        },
+
+	        // Otherwise as a last resort we fallback to attempting to enable the LunarG core layer
+	        {"VK_LAYER_LUNARG_core_validation"}};
+
+	for (auto &validation_layers : validation_layer_priority_list)
+	{
+		if (validate_layers(validation_layers, supported_instance_layers))
+		{
+			return validation_layers;
+		}
+
+		LOGW("Couldn't enable validation layers (see log for error) - falling back");
+	}
+
+	// Else return nothing
+	return {};
+}
+
 /**
  * @brief Initializes the Vulkan instance.
  *
@@ -186,7 +247,7 @@ void HPPHelloTriangle::init_instance(Context &                        context,
 
 #ifdef VKB_VALIDATION_LAYERS
 	// Determine the optimal validation layers to enable that are necessary for useful debugging
-	std::vector<const char *> optimal_validation_layers = vkb::get_optimal_validation_layers(supported_validation_layers);
+	std::vector<const char *> optimal_validation_layers = get_optimal_validation_layers(supported_validation_layers);
 	requested_validation_layers.insert(requested_validation_layers.end(), optimal_validation_layers.begin(), optimal_validation_layers.end());
 #endif
 
