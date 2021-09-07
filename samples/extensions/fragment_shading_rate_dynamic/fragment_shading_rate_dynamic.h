f50168d49363bc20fd3773b7abfa3859c5e36818
William Lohry <wflohry@wflohry.com>
2021-09-07 12:28:07 -0400

    Adding skeleton for dynamic fragment shading rate

---
+++ samples/extensions/fragment_shading_rate_dynamic/fragment_shading_rate_dynamic.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2021, Holochip
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This sample is demonstrates using a dynamic shading rate map generated each frame based on the
+ * frequency content of the previous frame.
+ */
+
+#pragma once
+
+#include "api_vulkan_sample.h"
+
+class FragmentShadingRateDynamic : public ApiVulkanSample
+{
+  public:
+};
+
+std::unique_ptr<vkb::VulkanSample> create_fragment_shading_rate_dynamic();
