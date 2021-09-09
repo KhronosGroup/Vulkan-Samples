a57a363fb801efc271509e292d74eaed70d690be
William Lohry <wflohry@wflohry.com>
2021-09-09 10:01:02 -0400

    Using fragment shading rate as template for code

--- samples/extensions/fragment_shading_rate_dynamic/fragment_shading_rate_dynamic.h
+++ samples/extensions/fragment_shading_rate_dynamic/fragment_shading_rate_dynamic.h
@@ -27,6 +27,83 @@
 class FragmentShadingRateDynamic : public ApiVulkanSample
 {
   public:
+    FragmentShadingRateDynamic();
+    ~FragmentShadingRateDynamic();
+    virtual bool prepare(vkb::Platform &platform) override;
+    virtual void resize(const uint32_t new_width, const uint32_t new_height) override;
+    virtual void request_gpu_features(vkb::PhysicalDevice &gpu) override;
+    virtual void render(float delta_time) override;
+    virtual void build_command_buffers() override;
+    virtual void setup_framebuffer() override;
+    virtual void setup_render_pass() override;
+    virtual void on_update_ui_overlay(vkb::Drawer &drawer) override;
+
+  private:
+    void create_shading_rate_attachment();
+    void invalidate_shading_rate_attachment();
+    void load_assets();
+    void prepare_uniform_buffers();
+    void update_uniform_buffers();
+    void setup_descriptor_set_layout();
+    void prepare_pipelines();
+    void setup_descriptor_pool();
+    void setup_descriptor_sets();
+    void draw();
+
+	bool enable_attachment_shading_rate = true;
+	bool color_shading_rate             = false;
+	bool display_skysphere              = true;
+
+	VkPhysicalDeviceFragmentShadingRatePropertiesKHR physical_device_fragment_shading_rate_properties{};
+	VkPhysicalDeviceFragmentShadingRateFeaturesKHR   enabled_physical_device_fragment_shading_rate_features{};
+
+	struct ShadingRateImage
+	{
+		VkImage        image  = VK_NULL_HANDLE;
+		VkDeviceMemory memory = VK_NULL_HANDLE;
+		VkImageView    view   = VK_NULL_HANDLE;
+	} shading_rate_image;
+
+	struct
+	{
+		Texture skysphere;
+		Texture scene;
+	} textures;
+
+	struct
+	{
+		std::unique_ptr<vkb::sg::SubMesh> skysphere;
+		std::unique_ptr<vkb::sg::SubMesh> scene;
+	} models;
+
+	struct
+	{
+		std::unique_ptr<vkb::core::Buffer> scene;
+	} uniform_buffers;
+
+	struct UBOScene
+	{
+		glm::mat4 projection;
+		glm::mat4 modelview;
+		glm::mat4 skysphere_modelview;
+		int32_t   color_shading_rate;
+	} ubo_scene;
+
+	VkPipelineLayout pipeline_layout;
+	struct Pipelines
+	{
+		VkPipeline skysphere;
+		VkPipeline sphere;
+	} pipelines;
+
+	VkDescriptorSetLayout descriptor_set_layout;
+	VkDescriptorSet       descriptor_set;
+
+	struct
+	{
+		glm::vec4 offset;
+		uint32_t  object_type;
+	} push_const_block;
 };
 
 std::unique_ptr<vkb::VulkanSample> create_fragment_shading_rate_dynamic();
