f5d48f54ecdf2125319f06e283264b60f21be38d
swinston <steve@holochip.com>
2021-09-21 11:56:43 -0700

    follow @cforfang's advice on explicitly showing one mipmap assertion to ensure this is clear.

--- samples/performance/bindless/bindless.cpp
+++ samples/performance/bindless/bindless.cpp
@@ -310,13 +310,14 @@ void BindlessResources::load_scene()
 		image->create_vk_image(*device);
 		Texture texture;
 		texture.n_mip_maps = uint32_t(image->get_mipmaps().size());
+        assert(texture.n_mip_maps == 1);
 		texture.image      = std::make_unique<vkb::core::Image>(*device,
                                                            image->get_extent(),
                                                            image->get_format(),
                                                            VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
                                                            VMA_MEMORY_USAGE_GPU_ONLY,
                                                            VK_SAMPLE_COUNT_1_BIT,
-                                                           texture.n_mip_maps,
+                                                           1,
                                                            1,
                                                            VK_IMAGE_TILING_OPTIMAL,
                                                            0);
