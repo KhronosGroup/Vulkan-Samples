1c7a549c04a4b42d1114e294e9ef9382438c2cca
swinston <steve@holochip.com>
2021-10-20 08:11:42 -0700

    fix the validation error reported in #378

--- samples/extensions/open_gl_interop/open_gl_interop.cpp
+++ samples/extensions/open_gl_interop/open_gl_interop.cpp
@@ -199,7 +199,10 @@ void OpenGLInterop::prepare_shared_resources()
 	}
 
 	{
+		VkExternalMemoryImageCreateInfo external_memory_image_create_info{VK_STRUCTURE_TYPE_EXTERNAL_MEMORY_IMAGE_CREATE_INFO};
+		external_memory_image_create_info.handleTypes = VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT_KHR;
 		VkImageCreateInfo imageCreateInfo{VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO};
+		imageCreateInfo.pNext         = &external_memory_image_create_info;
 		imageCreateInfo.imageType     = VK_IMAGE_TYPE_2D;
 		imageCreateInfo.format        = VK_FORMAT_R8G8B8A8_UNORM;
 		imageCreateInfo.mipLevels     = 1;
