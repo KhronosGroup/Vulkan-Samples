1dc8f52c293d4bd06d067e6883c3b38e3c44222f
asuessenbach <asuessenbach@nvidia.com>
2022-05-23 15:21:41 +0200

    Change vkb::core::HPPVulkanResource from a facade over vkb::core::VulkanResource to a self-contained class using vulkan.hpp

--- framework/core/hpp_vulkan_resource.h
+++ framework/core/hpp_vulkan_resource.h
@@ -17,7 +17,7 @@
 
 #pragma once
 
-#include <core/vulkan_resource.h>
+#include <vulkan/vulkan.hpp>
 
 namespace vkb
 {
@@ -25,31 +25,91 @@ namespace core
 {
 class HPPDevice;
 
-/**
- * @brief facade class around vkb::VulkanResource, providing a vulkan.hpp-based interface
- *
- * See vkb::VulkanResource for documentation
- */
-template <typename HPPHandle, typename Device = vkb::core::HPPDevice>
-class HPPVulkanResource : private vkb::core::VulkanResource<typename HPPHandle::NativeType, static_cast<VkObjectType>(HPPHandle::objectType), Device>
+namespace detail
+{
+void set_debug_name(const HPPDevice *device, vk::ObjectType object_type, uint64_t handle, const char *debug_name);
+}
+
+/// Inherit this for any Vulkan object with a handle of type `HPPHandle`.
+///
+/// This allows the derived class to store a Vulkan handle, and also a pointer to the parent vkb::core::Device.
+/// It also allow for adding debug data to any Vulkan object.
+template <typename HPPHandle, typename VKBDevice = vkb::core::HPPDevice>
+class HPPVulkanResource
 {
   public:
-	HPPVulkanResource(HPPHandle handle = nullptr, vkb::core::HPPDevice *device = nullptr) :
-	    vkb::core::VulkanResource<typename HPPHandle::NativeType, static_cast<VkObjectType>(HPPHandle::objectType), Device>(handle, device)
-	{}
-
-	HPPHandle const &get_handle() const
+	HPPVulkanResource(HPPHandle handle = nullptr, VKBDevice *device = nullptr) :
+	    handle{handle}, device{device}
 	{
-		return reinterpret_cast<HPPHandle const &>(
-		    vkb::core::VulkanResource<typename HPPHandle::NativeType, static_cast<VkObjectType>(HPPHandle::objectType), Device>::get_handle());
 	}
 
-  protected:
-	void set_handle(HPPHandle hppHandle)
+	HPPVulkanResource(const HPPVulkanResource &) = delete;
+	HPPVulkanResource &operator=(const HPPVulkanResource &) = delete;
+
+	HPPVulkanResource(HPPVulkanResource &&other) :
+	    handle(std::exchange(other.handle, {})), device(std::exchange(other.device, {}))
 	{
-		vkb::core::VulkanResource<typename HPPHandle::NativeType, static_cast<VkObjectType>(HPPHandle::objectType), Device>::handle =
-		    static_cast<typename HPPHandle::NativeType>(hppHandle);
+		set_debug_name(std::exchange(other.debug_name, {}));
 	}
+
+	HPPVulkanResource &operator=(HPPVulkanResource &&other)
+	{
+		handle = std::exchange(other.handle, {});
+		device = std::exchange(other.device, {});
+		set_debug_name(std::exchange(other.debug_name, {}));
+
+		return *this;
+	}
+
+	virtual ~HPPVulkanResource() = default;
+
+	inline vk::ObjectType get_object_type() const
+	{
+		return HPPHandle::NativeType;
+	}
+
+	inline VKBDevice &get_device() const
+	{
+		assert(device && "VKBDevice handle not set");
+		return *device;
+	}
+
+	inline const HPPHandle &get_handle() const
+	{
+		return handle;
+	}
+
+	inline const uint64_t get_handle_u64() const
+	{
+		// See https://github.com/KhronosGroup/Vulkan-Docs/issues/368 .
+		// Dispatchable and non-dispatchable handle types are *not* necessarily binary-compatible!
+		// Non-dispatchable handles _might_ be only 32-bit long. This is because, on 32-bit machines, they might be a typedef to a 32-bit pointer.
+		using UintHandle = typename std::conditional<sizeof(HPPHandle) == sizeof(uint32_t), uint32_t, uint64_t>::type;
+
+		return static_cast<uint64_t>(reinterpret_cast<UintHandle>(handle));
+	}
+
+	inline void set_handle(HPPHandle hdl)
+	{
+		assert(!handle && hdl);
+		handle = hdl;
+	}
+
+	inline const std::string &get_debug_name() const
+	{
+		return debug_name;
+	}
+
+	inline void set_debug_name(const std::string &name)
+	{
+		debug_name = name;
+		detail::set_debug_name(device, HPPHandle::NativeType, get_handle_u64(), debug_name.c_str());
+	}
+
+  private:
+	HPPHandle   handle;
+	VKBDevice * device;
+	std::string debug_name;
 };
 
 }        // namespace core
