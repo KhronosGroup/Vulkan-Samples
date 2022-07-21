0ea815b0e53da98ff5903da1ae3c5427b8a763df
Tom Atkinson <48598630+TomAtkinsonArm@users.noreply.github.com>
2022-07-21 10:26:29 +0100

    Visual Studio Folders (#496)
    * fix folder structure in Visual Studio
    
    * add interface library support
    
    * review changes

--- bldsys/cmake/test_helpers.cmake
+++ bldsys/cmake/test_helpers.cmake
@@ -23,6 +23,9 @@ endmacro()
 
 add_custom_target(vkb_tests)
 
+set_property(TARGET vkb_tests PROPERTY FOLDER "tests")
+
+
 function(vkb__register_tests)
     set(options)
     set(oneValueArgs NAME)
@@ -47,7 +50,11 @@ function(vkb__register_tests)
     add_executable(${TARGET_NAME} ${TARGET_SRC})
     target_link_libraries(${TARGET_NAME} PUBLIC Catch2::Catch2WithMain)
 
-    if(TARGET_LIBS)
+    target_compile_definitions(${TARGET_NAME} PUBLIC VKB_BUILD_TESTS)
+
+    set_property(TARGET ${TARGET_NAME} PROPERTY FOLDER "tests")
+
+    if (TARGET_LIBS)
         target_link_libraries(${TARGET_NAME} PUBLIC ${TARGET_LIBS})
     endif()
 
