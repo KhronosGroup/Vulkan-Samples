9e57eca622ab6cc6580c8756f195515b0786b70e
Gary Sweet <gsweet@broadcom.com>
2022-08-09 15:06:25 +0100

    Fix clang formatting

--- framework/platform/hpp_window.h
+++ framework/platform/hpp_window.h
@@ -34,9 +34,9 @@ class HPPWindow : private vkb::Window
 {
   public:
 	using vkb::Window::create_surface;
+	using vkb::Window::get_display_present_info;
 	using vkb::Window::get_extent;
 	using vkb::Window::get_window_mode;
-	using vkb::Window::get_display_present_info;
 
 	vk::SurfaceKHR create_surface(vkb::core::HPPInstance &instance)
 	{
