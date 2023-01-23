1a667937b6582db6ce3f7a112f0d9ca7cca1cd63
Andreas Süßenbach <asuessenbach@nvidia.com>
2023-01-23 17:13:45 +0100

    Refactored API sample hpp_compute_nbody. (#593)
    * Refactored API sample hpp_compute_nbody.
    
    * Resolve formatting issue.
    
    * Resolve build failures.

--- samples/api/hpp_compute_nbody/hpp_compute_nbody.h
+++ samples/api/hpp_compute_nbody/hpp_compute_nbody.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+/* Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -33,59 +33,55 @@
 class HPPComputeNBody : public HPPApiVulkanSample
 {
   public:
-	uint32_t num_particles;
-	uint32_t work_group_size  = 128;
-	uint32_t shared_data_size = 1024;
+	HPPComputeNBody();
+	~HPPComputeNBody();
 
-	struct
-	{
-		HPPTexture particle;
-		HPPTexture gradient;
-	} textures;
-
-	// Resources for the graphics part of the example
-	struct
-	{
-		std::unique_ptr<vkb::core::HPPBuffer> uniform_buffer;               // Contains scene matrices
-		vk::DescriptorSetLayout               descriptor_set_layout;        // Particle system rendering shader binding layout
-		vk::DescriptorSet                     descriptor_set;               // Particle system rendering shader bindings
-		vk::PipelineLayout                    pipeline_layout;              // Layout of the graphics pipeline
-		vk::Pipeline                          pipeline;                     // Particle rendering pipeline
-		vk::Semaphore                         semaphore;                    // Execution dependency between compute & graphic submission
-		uint32_t                              queue_family_index;
-		struct
-		{
-			glm::mat4 projection;
-			glm::mat4 view;
-			glm::vec2 screenDim;
-		} ubo;
-	} graphics;
-
+  private:
 	// Resources for the compute part of the example
-	struct
+	struct ComputeUBO
+	{                              // Compute shader uniform block object
+		float   delta_time;        // Frame delta time
+		int32_t particle_count;
+	};
+
+	struct Compute
 	{
-		std::unique_ptr<vkb::core::HPPBuffer> storage_buffer;               // (Shader) storage buffer object containing the particles
-		std::unique_ptr<vkb::core::HPPBuffer> uniform_buffer;               // Uniform buffer object containing particle system parameters
-		vk::Queue                             queue;                        // Separate queue for compute commands (queue family may differ from the one used for graphics)
-		vk::CommandPool                       command_pool;                 // Use a separate command pool (queue family may differ from the one used for graphics)
 		vk::CommandBuffer                     command_buffer;               // Command buffer storing the dispatch commands and barriers
-		vk::Semaphore                         semaphore;                    // Execution dependency between compute & graphic submission
-		vk::DescriptorSetLayout               descriptor_set_layout;        // Compute shader binding layout
+		vk::CommandPool                       command_pool;                 // Use a separate command pool (queue family may differ from the one used for graphics)
 		vk::DescriptorSet                     descriptor_set;               // Compute shader bindings
-		vk::PipelineLayout                    pipeline_layout;              // Layout of the compute pipeline
+		vk::DescriptorSetLayout               descriptor_set_layout;        // Compute shader binding layout
 		vk::Pipeline                          pipeline_calculate;           // Compute pipeline for N-Body velocity calculation (1st pass)
 		vk::Pipeline                          pipeline_integrate;           // Compute pipeline for euler integration (2nd pass)
-		vk::Pipeline                          blur;
-		vk::PipelineLayout                    pipeline_layout_blur;
-		vk::DescriptorSetLayout               descriptor_set_layout_blur;
-		vk::DescriptorSet                     descriptor_set_blur;
-		uint32_t                              queue_family_index;
-		struct ComputeUBO
-		{                              // Compute shader uniform block object
-			float   delta_time;        // Frame delta time
-			int32_t particle_count;
-		} ubo;
-	} compute;
+		vk::PipelineLayout                    pipeline_layout;              // Layout of the compute pipeline
+		vk::Queue                             queue;                        // Separate queue for compute commands (queue family may differ from the one used for graphics)
+		uint32_t                              queue_family_index = ~0;
+		vk::Semaphore                         semaphore;        // Execution dependency between compute & graphic submission
+		uint32_t                              shared_data_size = 1024;
+		std::unique_ptr<vkb::core::HPPBuffer> storage_buffer;        // (Shader) storage buffer object containing the particles
+		ComputeUBO                            ubo;
+		std::unique_ptr<vkb::core::HPPBuffer> uniform_buffer;        // Uniform buffer object containing particle system parameters
+		uint32_t                              work_group_size = 128;
+	};
+
+	// Resources for the graphics part of the example
+	struct GraphicsUBO
+	{
+		glm::mat4 projection;
+		glm::mat4 view;
+		glm::vec2 screenDim;
+	};
+
+	struct Graphics
+	{
+		vk::DescriptorSet                     descriptor_set;               // Particle system rendering shader bindings
+		vk::DescriptorSetLayout               descriptor_set_layout;        // Particle system rendering shader binding layout
+		vk::Pipeline                          pipeline;                     // Particle rendering pipeline
+		vk::PipelineLayout                    pipeline_layout;              // Layout of the graphics pipeline
+		uint32_t                              queue_family_index = ~0;
+		vk::Semaphore                         semaphore;        // Execution dependency between compute & graphic submission
+		GraphicsUBO                           ubo;
+		std::unique_ptr<vkb::core::HPPBuffer> uniform_buffer;        // Contains scene matrices
+	};
 
 	// SSBO particle declaration
 	struct Particle
@@ -94,26 +90,49 @@ class HPPComputeNBody : public HPPApiVulkanSample
 		glm::vec4 vel;        // xyz = velocity, w = gradient texture position
 	};
 
-	HPPComputeNBody();
-	~HPPComputeNBody();
-	virtual void request_gpu_features(vkb::core::HPPPhysicalDevice &gpu) override;
-	void         load_assets();
-	void         build_command_buffers() override;
-	void         build_compute_command_buffer();
-	void         prepare_storage_buffers();
-	void         setup_descriptor_pool();
-	void         setup_descriptor_set_layout();
-	void         setup_descriptor_set();
-	void         prepare_pipelines();
-	void         prepare_graphics();
-	void         prepare_compute();
-	void         prepare_uniform_buffers();
-	void         update_compute_uniform_buffers(float delta_time);
-	void         update_graphics_uniform_buffers();
-	void         draw();
-	bool         prepare(vkb::platform::HPPPlatform &platform) override;
-	virtual void render(float delta_time) override;
-	virtual bool resize(const uint32_t width, const uint32_t height) override;
+	struct Textures
+	{
+		HPPTexture gradient;
+		HPPTexture particle;
+	};
+
+  private:
+	// from platform::HPPApplication
+	bool prepare(vkb::platform::HPPPlatform &platform) override;
+	bool resize(const uint32_t width, const uint32_t height) override;
+
+	// from HPPVulkanSample
+	void request_gpu_features(vkb::core::HPPPhysicalDevice &gpu) override;
+
+	// from HPPApiVulkanSample
+	void build_command_buffers() override;
+	void render(float delta_time) override;
+
+	void build_compute_command_buffer();
+	void build_compute_transfer_command_buffer(vk::CommandBuffer command_buffer) const;
+	void build_copy_command_buffer(vk::CommandBuffer command_buffer, vk::Buffer staging_buffer, vk::DeviceSize buffer_size) const;
+	void create_compute_descriptor_set_layout();
+	void create_compute_pipeline_particle_integration();
+	void create_compute_pipeline_particle_movement();
+	void create_compute_pipelines();
+	void prepare_compute_storage_buffers();
+	void create_descriptor_pool();
+	void create_graphics_descriptor_set_layout();
+	void create_graphics_pipeline();
+	void draw();
+	void initializeCamera();
+	void load_assets();
+	void prepare_compute();
+	void prepare_graphics();
+	void update_compute_descriptor_set();
+	void update_compute_uniform_buffers(float delta_time);
+	void update_graphics_descriptor_set();
+	void update_graphics_uniform_buffers();
+
+  private:
+	Compute  compute;
+	Graphics graphics;
+	Textures textures;
 };
 
 std::unique_ptr<vkb::Application> create_hpp_compute_nbody();
