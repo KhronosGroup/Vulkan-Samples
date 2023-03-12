8f690b7120700b68e90141438aeff32da9a7fb44
swinston <steve@holochip.com>
2023-03-12 15:33:04 -0700

    first checkin of vk_video

---
+++ samples/extensions/video/VideoProcessor.h
@@ -0,0 +1,12 @@
+//
+// Created by swinston on 3/12/23.
+//
+
+#ifndef VULKAN_SAMPLES_VIDEOPROCESSOR_H
+#define VULKAN_SAMPLES_VIDEOPROCESSOR_H
+
+class VideoProcessor
+{
+};
+
+#endif        // VULKAN_SAMPLES_VIDEOPROCESSOR_H
