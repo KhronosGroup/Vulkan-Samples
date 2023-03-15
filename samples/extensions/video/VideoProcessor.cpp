fa0d16f416187683c85a834b91dd21318622e8a2
swinston <steve@holochip.com>
2023-03-15 09:14:59 -0700

    WIP

--- samples/extensions/video/VideoProcessor.cpp
+++ samples/extensions/video/VideoProcessor.cpp
@@ -3,3 +3,69 @@
 //
 
 #include "VideoProcessor.h"
+
+VkFormat VideoProcessor::GetFrameImageFormat(int32_t *width, int32_t *height, int32_t *bitDepth)
+{
+	return VK_FORMAT_R32_UINT;
+}
+int32_t VideoProcessor::GetWidth()
+{
+	return 0;
+}
+int32_t VideoProcessor::GetHeight()
+{
+	return 0;
+}
+int32_t VideoProcessor::GetBitDepth()
+{
+	return 0;
+}
+size_t VideoProcessor::OutputFrameToFile(DecodedFrame *Frame)
+{
+	return 0;
+}
+void VideoProcessor::Restart()
+{
+}
+bool VideoProcessor::StreamCompleted()
+{
+	return false;
+}
+int32_t VideoProcessor::ParserProcessNextDataChunk()
+{
+	return 0;
+}
+int32_t VideoProcessor::GetNextFrame(DecodedFrame *Frame, bool *endOfStream)
+{
+	return 0;
+}
+int32_t VideoProcessor::ReleaseDisplayedFrame(DecodedFrame *Frame)
+{
+	return 0;
+}
+VkResult VideoProcessor::CreateParser(const char *filename, VkVideoCodecOperationFlagBitsKHR vkCodecType, uint32_t defaultMinBufferSize, uint32_t bufferOffsetAlignment, uint32_t bufferSizeAlignment)
+{
+	return VK_ERROR_OUT_OF_DEVICE_MEMORY;
+}
+VkResult VideoProcessor::ParseVideoStreamData(const uint8_t *data, size_t size, size_t *VideoBytes, bool doPartialParsing, uint32_t flags, uint64_t timestamp)
+{
+	if (!vkParser) {
+		assert(!"Parser not initialized!");
+		return VK_ERROR_INITIALIZATION_FAILED;
+	}
+
+	VkParserSourceDataPacket packet = { 0 };
+	packet.payload = pData;
+	packet.payload_size = size;
+	packet.flags = flags;
+	if (timestamp) {
+		packet.flags |= VK_PARSER_PKT_TIMESTAMP;
+	}
+	packet.timestamp = timestamp;
+	if (!pData || size == 0) {
+		packet.flags |= VK_PARSER_PKT_ENDOFSTREAM;
+	}
+
+	return m_vkParser->ParseVideoData(&packet, pnVideoBytes, doPartialParsing);
+
+}
--- samples/extensions/video/VideoProcessor.h
+++ samples/extensions/video/VideoProcessor.h
@@ -5,8 +5,29 @@
 #ifndef VULKAN_SAMPLES_VIDEOPROCESSOR_H
 #define VULKAN_SAMPLES_VIDEOPROCESSOR_H
 
+#include "common/vk_common.h"
+
+class DecodedFrame;
+
 class VideoProcessor
 {
+	VkFormat GetFrameImageFormat(int32_t * width, int32_t * height, int32_t * bitDepth);
+	int32_t GetWidth();
+	int32_t GetHeight();
+	int32_t GetBitDepth();
+	size_t OutputFrameToFile(DecodedFrame* Frame);
+	void Restart();
+	bool StreamCompleted();
+	int32_t ParserProcessNextDataChunk();
+	int32_t GetNextFrame(DecodedFrame * Frame, bool* endOfStream);
+	int32_t ReleaseDisplayedFrame(DecodedFrame * Frame);
+	VkResult CreateParser(const char* filename,
+	                      VkVideoCodecOperationFlagBitsKHR vkCodecType,
+	                      uint32_t defaultMinBufferSize,
+	                      uint32_t bufferOffsetAlignment,
+	                      uint32_t bufferSizeAlignment);
+	VkResult ParseVideoStreamData(const uint8_t* data, size_t size, size_t *VideoBytes, bool doPartialParsing, uint32_t flags, uint64_t timestamp);
+
 };
 
 #endif        // VULKAN_SAMPLES_VIDEOPROCESSOR_H
