b1b71762f95089c5bd3726a70f03cd8cc2952104
Tom Atkinson <tja.atkinson@gmail.com>
2023-05-05 02:33:59 +0100

    Added v2.0 component structure

---
+++ components/common/tests/strings.test.cpp
@@ -0,0 +1,46 @@
+/* Copyright (c) 2023, Thomas Atkinson
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <components/common/error.hpp>
+
+VKBP_DISABLE_WARNINGS()
+#include <catch2/catch_test_macros.hpp>
+VKBP_ENABLE_WARNINGS()
+
+#include <components/common/strings.hpp>
+
+using namespace vkb;
+
+TEST_CASE("vkb::replace_all", "[common]")
+{
+	REQUIRE(replace_all("/././", "./", "/") == "///");
+	REQUIRE(replace_all("vulkanvulkanvulkan", "vulkan", "kan") == "kankankan");
+}
+
+TEST_CASE("vkb::trim_right", "[common]")
+{
+	REQUIRE(trim_right("hello   ", " ") == "hello");
+	REQUIRE(trim_right("hello   ignore", " ") == "hello   ignore");
+	REQUIRE(trim_right("hellocomplex", "complex") == "h");        // remember we are trimming a set
+}
+
+TEST_CASE("vkb::trim_left", "[common]")
+{
+	REQUIRE(trim_left("   hello", " ") == "hello");
+	REQUIRE(trim_left("ignore   hello", " ") == "ignore   hello");
+	REQUIRE(trim_left("complexhello", "complex") == "hello");        // remember we are trimming a set until the first non-match
+}
