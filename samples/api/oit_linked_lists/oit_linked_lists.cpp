a3f1985e6cf2697f9d5a120e526270c37381d8a1
Maximilien <maximiliendagois@gmail.com>
2023-10-08 15:16:24 +0900

    Generated a sample template with the generation script

---
+++ samples/api/oit_linked_lists/oit_linked_lists.cpp
@@ -0,0 +1,64 @@
+/* Copyright (c) 2019-2023, Arm Limited and Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "oit_linked_lists.h"
+
+#include "common/vk_common.h"
+#include "gltf_loader.h"
+#include "gui.h"
+#include "platform/filesystem.h"
+#include "platform/platform.h"
+#include "rendering/subpasses/forward_subpass.h"
+#include "stats/stats.h"
+
+oit_linked_lists::oit_linked_lists()
+{
+}
+
+bool oit_linked_lists::prepare(const vkb::ApplicationOptions &options)
+{
+	if (!VulkanSample::prepare(options))
+	{
+		return false;
+	}
+
+	// Load a scene from the assets folder
+	load_scene("scenes/sponza/Sponza01.gltf");
+
+	// Attach a move script to the camera component in the scene
+	auto &camera_node = vkb::add_free_camera(*scene, "main_camera", get_render_context().get_surface_extent());
+	auto  camera      = &camera_node.get_component<vkb::sg::Camera>();
+
+	// Example Scene Render Pipeline
+	vkb::ShaderSource vert_shader("base.vert");
+	vkb::ShaderSource frag_shader("base.frag");
+	auto              scene_subpass   = std::make_unique<vkb::ForwardSubpass>(get_render_context(), std::move(vert_shader), std::move(frag_shader), *scene, *camera);
+	auto              render_pipeline = vkb::RenderPipeline();
+	render_pipeline.add_subpass(std::move(scene_subpass));
+	set_render_pipeline(std::move(render_pipeline));
+
+	// Add a GUI with the stats you want to monitor
+	stats->request_stats({/*stats you require*/});
+	gui = std::make_unique<vkb::Gui>(*this, *window, stats.get());
+
+	return true;
+}
+
+std::unique_ptr<vkb::VulkanSample> create_oit_linked_lists()
+{
+	return std::make_unique<oit_linked_lists>();
+}
---
+++ samples/api/oit_linked_lists/oit_linked_lists.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2019-2023, Arm Limited and Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "rendering/render_pipeline.h"
+#include "scene_graph/components/camera.h"
+#include "vulkan_sample.h"
+
+class oit_linked_lists : public vkb::VulkanSample
+{
+  public:
+	oit_linked_lists();
+
+	virtual bool prepare(const vkb::ApplicationOptions &options) override;
+
+	virtual ~oit_linked_lists() = default;
+};
+
+std::unique_ptr<vkb::VulkanSample> create_oit_linked_lists();
