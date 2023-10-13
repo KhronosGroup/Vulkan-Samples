a930066592f164e433dbdbef6d2507cd068b3520
pawel-jastrzebski-mobica <pawel.jastrzebski@mobica.com>
2023-10-13 13:56:20 +0200

    Add initial test framework.
    Signed-off-by: Pawel Jastrzebski <pawel.jastrzebski@mobica.com>

---
+++ samples/extensions/dynamic_state3_multisample_rasterization/dynamic_state3_multisample_rasterization.h
@@ -0,0 +1,42 @@
+/* Copyright (c) 2023, Arm Limited and Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "api_vulkan_sample.h"
+
+class DynamicState3MultisampleRasterization : public ApiVulkanSample
+{
+  public:
+	DynamicState3MultisampleRasterization();
+	virtual ~DynamicState3MultisampleRasterization();
+
+	// Create pipeline
+	void prepare_pipelines();
+
+	// Override basic framework functionality
+	void build_command_buffers() override;
+	void render(float delta_time) override;
+	bool prepare(const vkb::ApplicationOptions &options) override;
+
+  private:
+	// Sample specific data
+	VkPipeline       sample_pipeline{};
+	VkPipelineLayout sample_pipeline_layout{};
+};
+
+std::unique_ptr<vkb::VulkanSample> create_dynamic_state3_multisample_rasterization();
