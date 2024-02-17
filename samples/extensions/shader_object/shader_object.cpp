7887bb89719229d6dbaa0d19ec26ded81886fd0c
Sascha Willems <webmaster@saschawillems.de>
2024-02-17 17:04:28 +0100

    Set color blend euqation Required as per spec Fixes #917

--- samples/extensions/shader_object/shader_object.cpp
+++ samples/extensions/shader_object/shader_object.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright 2023 Nintendo
- * Copyright 2023, Sascha Willems
+ * Copyright 2023-2024 Nintendo
+ * Copyright 2023-2024, Sascha Willems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1431,6 +1431,10 @@ void ShaderObject::set_initial_state(VkCommandBuffer cmd)
 
 	// Rasterization is always enabled
 	vkCmdSetRasterizerDiscardEnableEXT(cmd, VK_FALSE);
+	
+	// This also requires setting blend equations
+	VkColorBlendEquationEXT colorBlendEquationEXT{};
+	vkCmdSetColorBlendEquationEXT(cmd, 0, 1, &colorBlendEquationEXT);
 
 	{
 		// Setup vertex input with position, normals, and uv
