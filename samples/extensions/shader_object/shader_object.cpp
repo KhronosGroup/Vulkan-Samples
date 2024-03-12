cd249f6e4b7e8a64bb83c269907d6ba0835b3b05
Sascha Willems <webmaster@saschawillems.de>
2024-03-12 17:16:14 +0100

    Remove merge-conflict left-over

--- samples/extensions/shader_object/shader_object.cpp
+++ samples/extensions/shader_object/shader_object.cpp
@@ -1,11 +1,6 @@
 /*
-<<<<<<< HEAD
  * Copyright 2023-2024 Nintendo
  * Copyright 2023-2024, Sascha Willems
-=======
- * Copyright 2023-2024-2024 Nintendo
- * Copyright 2023-2024-2024, Sascha Willems
->>>>>>> main
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
