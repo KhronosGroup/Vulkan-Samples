200b1e0bee6699daf29a5f14145ad3423b421166
Gary Sweet <20819159+gary-sweet@users.noreply.github.com>
2024-03-14 15:05:02 +0000

    Fix validation errors about using unsupported linear filtering (#924)
    * Resolve validation errors about unsupported linear filtering
    
    Most of the samples, and parts of the framework, just build samplers using
    linear filtering and mipmap-modes. This is only guaranteed to be supported
    for a handful of formats. On my hardware this results in a large number of
    validation errors.
    
    This change adds some helper functions to allow the filtering modes that
    are preferred to be checked against the format that will be used and
    drop back to nearest filtering if required.
    
    * Fix copyright headers
    
    * Fix commit check after rebase

--- samples/extensions/open_cl_interop_arm/open_cl_interop_arm.cpp
+++ samples/extensions/open_cl_interop_arm/open_cl_interop_arm.cpp
@@ -384,7 +384,7 @@ void OpenCLInteropArm::prepare_pipelines()
 	vertex_input_state.vertexBindingDescriptionCount        = vkb::to_u32(vertex_input_bindings.size());
 	vertex_input_state.pVertexBindingDescriptions           = vertex_input_bindings.data();
 	vertex_input_state.vertexAttributeDescriptionCount      = vkb::to_u32(
-	         vertex_input_attributes.size());
+        vertex_input_attributes.size());
 	vertex_input_state.pVertexAttributeDescriptions = vertex_input_attributes.data();
 
 	VkGraphicsPipelineCreateInfo pipeline_create_info =
@@ -499,10 +499,15 @@ void OpenCLInteropArm::prepare_shared_resources()
 	info.memory = shared_texture.memory;
 	VK_CHECK(vkGetMemoryAndroidHardwareBufferANDROID(device_handle, &info, &shared_texture.hardware_buffer));
 
+	// Calculate valid filter and mipmap modes
+	VkFilter            filter      = VK_FILTER_LINEAR;
+	VkSamplerMipmapMode mipmap_mode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+	vkb::make_filters_valid(get_device().get_gpu().get_handle(), image_create_info.format, &filter, &mipmap_mode);
+
 	VkSamplerCreateInfo sampler_create_info{VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO};
-	sampler_create_info.magFilter   = VK_FILTER_LINEAR;
-	sampler_create_info.minFilter   = VK_FILTER_LINEAR;
-	sampler_create_info.mipmapMode  = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+	sampler_create_info.magFilter   = filter;
+	sampler_create_info.minFilter   = filter;
+	sampler_create_info.mipmapMode  = mipmap_mode;
 	sampler_create_info.maxLod      = (float) 1;
 	sampler_create_info.borderColor = VK_BORDER_COLOR_FLOAT_OPAQUE_WHITE;
 	vkCreateSampler(device_handle, &sampler_create_info, nullptr, &shared_texture.sampler);
