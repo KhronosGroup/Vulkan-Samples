4ed5efce46e7a7f016005cddecd36ea41e1d8cb2
Sascha Willems <webmaster@saschawillems.de>
2024-03-15 12:29:18 +0100

    Add new argument to custom instance creation so that extensions enabled in a sample will be marked as enabled in the instance class This is required to make other parts of the framework properly work with samples that do custom instance creation

--- framework/core/instance.h
+++ framework/core/instance.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2023, Arm Limited and Contributors
+/* Copyright (c) 2018-2024, Arm Limited and Contributors
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -63,8 +63,10 @@ class Instance
 	/**
 	 * @brief Queries the GPUs of a VkInstance that is already created
 	 * @param instance A valid VkInstance
+	 * @param enabled_extensions List of extensions that have been enabled, used for following checks e.g. during device creation
 	 */
-	Instance(VkInstance instance);
+	Instance(VkInstance                       instance,
+	         const std::vector<const char *> &required_extensions = {});
 
 	Instance(const Instance &) = delete;
 
