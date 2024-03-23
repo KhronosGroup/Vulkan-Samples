e4b1a35c97f48fa8505cf5f3b8c0fe131c209cab
gpx1000 <gpx1000@gmail.com>
2024-03-22 21:20:17 -0700

    fix CMake less than 3.29 and MoltenVK.framework.

--- bldsys/cmake/module/FindVulkan.cmake
+++ bldsys/cmake/module/FindVulkan.cmake
@@ -479,7 +479,7 @@ endif()
 if(MoltenVK IN_LIST Vulkan_FIND_COMPONENTS)
     # CMake has a bug in 3.28 that doesn't handle xcframeworks.  Do it by hand for now.
     if(CMAKE_SYSTEM_NAME STREQUAL "iOS" AND CMAKE_VERSION VERSION_LESS 3.29)
-        LIST(APPEND _Vulkan_hint_library_search_paths ${_Vulkan_hint_library_search_paths}/MoltenVK.xcframework/ios-arm64)
+        set( _Vulkan_hint_library_search_paths ${Vulkan_Target_SDK}/ios/lib/MoltenVK.xcframework/ios-arm64)
     endif ()
     find_library(Vulkan_MoltenVK_LIBRARY
             NAMES MoltenVK
