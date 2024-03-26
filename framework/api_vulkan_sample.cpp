b77ad5ae11f925669b69d24912ade19a85a32884
Sascha Willems <webmaster@saschawillems.de>
2024-03-26 20:06:23 +0100

    Add new overload for loading shadders
    Separates folder and shader file name
    Will be the base for shader language selection

--- framework/api_vulkan_sample.cpp
+++ framework/api_vulkan_sample.cpp
@@ -437,6 +437,30 @@ VkPipelineShaderStageCreateInfo ApiVulkanSample::load_shader(const std::string &
 	return shader_stage;
 }
 
+VkPipelineShaderStageCreateInfo ApiVulkanSample::load_shader(const std::string &sample_folder_name, const std::string &shader_filename, VkShaderStageFlagBits stage)
+{
+	// @todo: Adjust depending on global shader language selection
+	vkb::ShaderSourceLanguage src_language  = vkb::ShaderSourceLanguage::HLSL;
+	std::string               shader_folder = (src_language == vkb::ShaderSourceLanguage::HLSL ? "hlsl" : "glsl");
+	// @todo: build full name based on global shader selection
+	std::string full_file_name = sample_folder_name + "/" + shader_folder + "/" + shader_filename;
+	if ((src_language == vkb::ShaderSourceLanguage::HLSL) || (src_language == vkb::ShaderSourceLanguage::SPV))
+	{
+		// @todo: Find a better solution
+		src_language = vkb::ShaderSourceLanguage::SPV;
+		full_file_name += ".spv";
+	}
+
+	VkPipelineShaderStageCreateInfo shader_stage = {};
+	shader_stage.sType                           = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	shader_stage.stage                           = stage;
+	shader_stage.module                          = vkb::load_shader(full_file_name, get_device().get_handle(), stage, src_language);
+	shader_stage.pName                           = "main";
+	assert(shader_stage.module != VK_NULL_HANDLE);
+	shader_modules.push_back(shader_stage.module);
+	return shader_stage;
+}
+
 void ApiVulkanSample::update_overlay(float delta_time, const std::function<void()> &additional_ui)
 {
 	if (has_gui())
