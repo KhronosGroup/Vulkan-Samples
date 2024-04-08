2005d5394c6d0405f9139bb58e351e9f74c0c2fc
Sascha Willems <webmaster@saschawillems.de>
2024-04-08 17:28:51 +0200

    Set color blend equation state in shader object sample (validation fix) (#918)
    * Set color blend euqation
    Required as per spec
    Fixes #917
    
    * Initialize values
    
    * Clang format fix
    
    * Remove merge-conflict left-over

--- samples/extensions/shader_object/shader_object.cpp
+++ samples/extensions/shader_object/shader_object.cpp
@@ -1441,6 +1441,10 @@ void ShaderObject::set_initial_state(VkCommandBuffer cmd)
 	// Rasterization is always enabled
 	vkCmdSetRasterizerDiscardEnableEXT(cmd, VK_FALSE);
 
+	// This also requires setting blend equations
+	VkColorBlendEquationEXT colorBlendEquationEXT{};
+	vkCmdSetColorBlendEquationEXT(cmd, 0, 1, &colorBlendEquationEXT);
+
 	{
 		// Setup vertex input with position, normals, and uv
 		const VkVertexInputBindingDescription2EXT vertex_binding[] =
