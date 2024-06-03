c6844fa67380b6f0d858b1b8e30d35b23601a247
Tom Atkinson <tja.atkinson@gmail.com>
2024-06-03 16:55:07 +0100

    remove unused tests (#1057)

--- app/apps/apps.h
+++ app/apps/apps.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020-2021, Arm Limited and Contributors
+/* Copyright (c) 2020-2024, Arm Limited and Contributors
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -29,7 +29,7 @@
  *
  *	Vulkan Samples currently has two types of apps, Samples and Tests. These apps work from the same interface AppInfo.
  *	Samples and Tests are categorised into namespaces samples and tests respectively. Each name space has a method to retrieve
- *  the AppInfo interface version of the app (tests::get_apps, samples::get_apps). 
+ *  the AppInfo interface version of the app (tests::get_apps, samples::get_apps).
  *
  */
 
@@ -70,36 +70,24 @@ class SampleInfo : public AppInfo
 	std::vector<std::string> tags;
 };
 
-/*
- *  Tests - Used to test Vulkan Samples functionality
- */
-
-class TestInfo : public AppInfo
-{
-  public:
-	TestInfo(const std::string &id, const CreateFunc &create) :
-	    AppInfo(id, create)
-	{}
-};
-
 /**
  * @brief Get a specific app
- * 
+ *
  * @param id ID of a specific app
- * @return const std::vector<AppInfo *> 
+ * @return const std::vector<AppInfo *>
  */
 AppInfo *get_app(const std::string &id);
 
 /**
  * @brief Get all apps
- * 
+ *
  * @return const std::vector<AppInfo *> A list of all apps
  */
 std::vector<AppInfo *> get_apps();
 
 /**
  * @brief Get all samples
- * 
+ *
  * @param categories If not empty the lists will include samples that match on of the categories requested
  * @param tags If not empty the lists will include samples that match on of the tags requested
  * @return std::vector<AppInfo *> A list of samples
@@ -107,11 +95,4 @@ std::vector<AppInfo *> get_apps();
 std::vector<AppInfo *> get_samples(const std::vector<std::string> &categories = {}, const std::vector<std::string> &tags = {});
 
 SampleInfo *get_sample(const std::string &id);
-
-/**
- * @brief Get all tests
- * 
- * @return std::vector<AppInfo *> A list of Tests
- */
-std::vector<AppInfo *> get_tests();
 }        // namespace apps
