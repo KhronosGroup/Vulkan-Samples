021b8f9bc5af99d2a36af8278c10e8bcc3000b88
Bryce Young <bryce.young@mobica.com>
2024-06-04 10:17:49 +0100

    Fixes for timeline semaphore

--- samples/extensions/timeline_semaphore/timeline_semaphore.h
+++ samples/extensions/timeline_semaphore/timeline_semaphore.h
@@ -18,98 +18,119 @@
 #pragma once
 
 #include "api_vulkan_sample.h"
-#include <memory>
-#include <mutex>
-#include <thread>
-#include <vector>
 
 class TimelineSemaphore : public ApiVulkanSample
 {
   public:
-	TimelineSemaphore();
-	~TimelineSemaphore();
+	static const uint32_t NumAsyncFrames = 2;
 
-  private:
-	virtual void request_gpu_features(vkb::PhysicalDevice &gpu) override;
-	virtual void render(float delta_time) override;
-	virtual void build_command_buffers() override;
-	virtual void on_update_ui_overlay(vkb::Drawer &drawer) override;
-	virtual bool prepare(const vkb::ApplicationOptions &options) override;
-	virtual void finish() override;
-
-	void create_resources();
-	void create_pipelines();
-	void create_compute_pipeline();
-	void create_graphics_pipeline();
-
-	struct Pipelines
+	// Resources for the graphics worker
+	struct GraphicsResources
 	{
-		VkPipelineLayout compute_pipeline_layout{};
-		VkPipelineLayout graphics_pipeline_layout{};
-		VkPipeline       visualize_pipeline{};
-		VkPipeline       compute_update_pipeline{};
-		VkPipeline       compute_mutate_pipeline{};
-		VkPipeline       compute_init_pipeline{};
-	} pipelines;
+		VkQueue         queue;
+		VkCommandPool   command_pool;
+		VkCommandBuffer command_buffer;
 
-	enum
+		VkPipelineLayout pipeline_layout;
+		VkPipeline       pipeline;
+
+		uint32_t queue_family_index;
+	} graphics;
+
+	// Resources for the compute worker
+	struct ComputeResources
 	{
-		NumAsyncFrames = 2
-	};
+		VkQueue         queue;
+		VkCommandPool   command_pool;
+		VkCommandBuffer command_buffer;
 
-	struct Descriptors
+		VkPipelineLayout pipeline_layout;
+		VkPipeline       init_pipeline;
+		VkPipeline       update_pipeline;
+		VkPipeline       mutate_pipeline;
+
+		vkb::Timer timer;
+		uint32_t   queue_family_index;
+
+		enum CommandType
+		{
+			init,
+			update,
+			mutate
+		};
+	} compute;
+
+	// Resources used by both workers for storing/sampling images
+	struct SharedResources
 	{
 		VkDescriptorSetLayout storage_layout;
 		VkDescriptorSetLayout sampled_layout;
 		VkDescriptorSet       storage_images[NumAsyncFrames];
 		VkDescriptorSet       sampled_images[NumAsyncFrames];
 		VkDescriptorPool      descriptor_pool;
-	} descriptors{};
 
-	std::unique_ptr<vkb::core::Sampler>   immutable_sampler;
-	std::unique_ptr<vkb::core::Image>     images[NumAsyncFrames];
-	std::unique_ptr<vkb::core::ImageView> image_views[NumAsyncFrames];
-
-	VkQueue    async_queue{VK_NULL_HANDLE};
-	void       prepare_queue();
-	std::mutex submission_lock;
+		std::unique_ptr<vkb::core::Sampler>   immutable_sampler;
+		std::unique_ptr<vkb::core::Image>     images[NumAsyncFrames];
+		std::unique_ptr<vkb::core::ImageView> image_views[NumAsyncFrames];
+	} shared;
 
 	struct Timeline
 	{
-		VkSemaphore semaphore;
-		uint64_t    timeline;
-	};
-	Timeline main_thread_timeline{}, async_compute_timeline{};
+		// The stages of the timeline are enumerated, to make it easier to read which stage we are signalling/waiting on, and to allow
+		// the stages to be reused without needing to recreate the semaphore.
+		enum Stages
+		{
+			prepare = 1,        // Worker threads can begin preparing command buffers for submission
+			submit,             // Worker threads can submit their command buffers,
+			draw,               // The graphics worker can draw the current frame
+			present,            // The main thread can present the frame to the display
+			MAX_STAGES
+		};
 
-	struct TimelineLock
-	{
-		std::condition_variable cond;
-		std::mutex              lock;
-		uint64_t                pending_timeline;
-	};
-	TimelineLock main_thread_timeline_lock{}, async_compute_timeline_lock{};
+		VkSemaphore semaphore;
+		uint64_t    frame;        // Number of iterations through the timeline stages
+	} timeline;
 
 	struct TimelineWorker
 	{
 		std::thread      thread;
 		std::atomic_bool alive;
 	};
-	TimelineWorker async_compute_worker;
-	void           create_timeline_semaphores();
-	void           create_timeline_semaphore(Timeline &timeline);
 
-	void        create_timeline_workers();
-	static void create_timeline_worker(TimelineWorker &worker, std::function<void()> thread_func);
+	TimelineWorker graphics_worker, compute_worker;
 
-	void async_compute_loop();
+	TimelineSemaphore();
+	~TimelineSemaphore();
 
-	void signal_timeline_gpu(VkQueue queue, const Timeline &timeline, TimelineLock &lock);
-	void wait_timeline_gpu(VkQueue queue, const Timeline &timeline, TimelineLock &lock);
-	void wait_timeline_cpu(const Timeline &timeline);
-	void signal_timeline_cpu(const Timeline &timeline, TimelineLock &lock);
-	void update_pending(TimelineLock &lock, uint64_t timeline);
-	void wait_pending_in_order_queue(TimelineLock &lock, uint64_t timeline);
-	void wait_pending(TimelineLock &lock, uint64_t timeline);
+	void setup_shared_resources();
+	void build_command_buffers() override;
+
+	// Timeline operations
+	void     create_timeline_semaphore();
+	void     start_timeline_workers();
+	void     finish_timeline_workers();
+	void     signal_next_frame();
+	void     wait_for_next_frame();
+	void     signal_timeline(Timeline::Stages stage);
+	void     wait_on_timeline(Timeline::Stages stage);
+	uint64_t get_timeline_stage_value(Timeline::Stages stage);
+
+	// Compute Work
+	void do_compute_work();
+	void setup_compute_pipeline();
+	void setup_compute_resources();
+	void build_compute_command_buffers(const ComputeResources::CommandType type, const float elapsed = 0.0f);
+
+	// Graphics Work
+	void do_graphics_work();
+	void setup_graphics_resources();
+	void setup_graphics_pipeline();
+	void build_graphics_command_buffer();
+
+	virtual void request_gpu_features(vkb::PhysicalDevice &gpu) override;
+	virtual bool prepare(const vkb::ApplicationOptions &options) override;
+	virtual void render(float delta_time) override;
+	virtual bool resize(const uint32_t width, const uint32_t height) override;
 };
 
-std::unique_ptr<vkb::VulkanSample<vkb::BindingType::C>> create_timeline_semaphore();
+std::unique_ptr<vkb::Application> create_timeline_semaphore();
