434c9cd1b9dee86dd24d7462c202360db6b158e9
Sascha Willems <webmaster@saschawillems.de>
2024-06-08 11:40:55 +0200

    Added sample for VK_KHR_ray_tracing_position_fetch
    Work-in-progress

---
+++ samples/extensions/ray_tracing_position_fetch/ray_tracing_position_fetch.h
@@ -0,0 +1,83 @@
+/* Copyright (c) 2023, Sascha Willems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "api_vulkan_sample.h"
+#include "glsl_compiler.h"
+#include <core/acceleration_structure.h>
+
+class RayTracingPositionFetch : public ApiVulkanSample
+{
+  public:
+	VkPhysicalDeviceRayTracingPipelinePropertiesKHR  ray_tracing_pipeline_properties{};
+	VkPhysicalDeviceAccelerationStructureFeaturesKHR acceleration_structure_features{};
+
+	std::unique_ptr<vkb::core::AccelerationStructure> bottom_level_acceleration_structure{nullptr};
+	std::unique_ptr<vkb::core::AccelerationStructure> top_level_acceleration_structure{nullptr};
+
+	std::unique_ptr<vkb::core::Buffer>                vertex_buffer;
+	std::unique_ptr<vkb::core::Buffer>                index_buffer;
+	std::vector<VkRayTracingShaderGroupCreateInfoKHR> shader_groups{};
+
+	std::unique_ptr<vkb::core::Buffer> raygen_shader_binding_table;
+	std::unique_ptr<vkb::core::Buffer> miss_shader_binding_table;
+	std::unique_ptr<vkb::core::Buffer> hit_shader_binding_table;
+
+	struct StorageImage
+	{
+		VkDeviceMemory memory;
+		VkImage        image;
+		VkImageView    view;
+		VkFormat       format;
+		uint32_t       width;
+		uint32_t       height;
+	} storage_image{};
+
+	struct UniformData
+	{
+		glm::mat4 view_inverse{1.0f};
+		glm::mat4 proj_inverse{1.0f};
+	} uniform_data;
+	std::unique_ptr<vkb::core::Buffer> ubo;
+
+	VkPipeline            pipeline{VK_NULL_HANDLE};
+	VkPipelineLayout      pipeline_layout{VK_NULL_HANDLE};
+	VkDescriptorSet       descriptor_set{VK_NULL_HANDLE};
+	VkDescriptorSetLayout descriptor_set_layout{VK_NULL_HANDLE};	
+
+	RayTracingPositionFetch();
+	virtual ~RayTracingPositionFetch();
+
+	void         request_gpu_features(vkb::PhysicalDevice &gpu) override;
+	uint64_t     get_buffer_device_address(VkBuffer buffer);
+	void         create_storage_image();
+	void         create_bottom_level_acceleration_structure();
+	void         create_top_level_acceleration_structure();
+	void         create_scene();
+	void         create_shader_binding_tables();
+	void         create_descriptor_sets();
+	void         create_ray_tracing_pipeline();
+	void         create_uniform_buffer();
+	void         build_command_buffers() override;
+	void         update_uniform_buffers();
+	void         draw();
+	bool         prepare(const vkb::ApplicationOptions &options) override;
+	virtual void render(float delta_time) override;
+};
+
+std::unique_ptr<vkb::VulkanSample<vkb::BindingType::C>> create_ray_tracing_position_fetch();
---
+++ shaders/ray_tracing_position_fetch/hlsl/closesthit.rchit.hlsl
@@ -0,0 +1,67 @@
+/* Copyright (c) 2024, Sascha Willems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+struct Attributes
+{
+    float2 bary;
+};
+
+struct Payload
+{
+    [[vk::location(0)]] float3 hitValue;
+};
+
+struct UBO
+{
+    float4x4 viewInverse;
+    float4x4 projInverse;
+    float4 lightPos;
+};
+ConstantBuffer<UBO> ubo : register(b2);
+
+// We need to use special syntax for SPIR-V inlines
+#define HitTriangleVertexPositionsKHR 5335
+#define RayTracingPositionFetchKHR 5336
+
+[[vk::ext_extension("SPV_KHR_ray_tracing_position_fetch")]]
+[[vk::ext_capability(RayTracingPositionFetchKHR)]]
+[[vk::ext_builtin_input(HitTriangleVertexPositionsKHR)]]
+const static float3 gl_HitTriangleVertexPositions[3];
+
+[shader("closesthit")]
+void main(inout Payload p, in Attributes attribs)
+{
+	// We need the barycentric coordinates to calculate data for the current position
+    const float3 barycentricCoords = float3(1.0f - attribs.bary.x - attribs.bary.y, attribs.bary.x, attribs.bary.y);
+
+	// With VK_KHR_ray_tracing_position_fetch we can access the vertices for the hit triangle in the shader
+
+    float3 vertexPos0 = gl_HitTriangleVertexPositions[0];
+    float3 vertexPos1 = gl_HitTriangleVertexPositions[1];
+    float3 vertexPos2 = gl_HitTriangleVertexPositions[2];
+    float3 currentPos = vertexPos0 * barycentricCoords.x + vertexPos1 * barycentricCoords.y + vertexPos2 * barycentricCoords.z;
+
+	// Calcualte the normal from above values
+    float3 normal = normalize(cross(vertexPos1 - vertexPos0, vertexPos2 - vertexPos0));
+    normal = normalize(mul(float4(normal, 1.0), WorldToObject4x3()));
+
+	// Basic lighting
+    float3 lightDir = normalize(ubo.lightPos.xyz - currentPos);
+    float diffuse = max(dot(normal, lightDir), 0.0);
+
+    p.hitValue.rgb = 0.1 + diffuse;
+}
