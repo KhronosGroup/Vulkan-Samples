5a888ded3b5e9036cb3fd0016eff1c421a6734f3
Iago Calvo Lista <iago.calvolista@arm.com>
2024-07-08 09:32:17 +0100

    Fix support for --headless.
    Now --headless will create a swap_chain and a surface, now the feature will work correctly.
    Remove headless parameter from Instance::Instance and VulkanSample<>::create_instance.
    VK_EXT_HEADLESS_SURFACE_EXTENSION_NAME  added in HeadlessWindow::get_required_surface_extensions like other window types
    Added an assert if headless is enabled to hello_triangle and hpp_hello_triangle

--- samples/api/hello_triangle/hello_triangle.cpp
+++ samples/api/hello_triangle/hello_triangle.cpp
@@ -1070,7 +1070,9 @@ HelloTriangle::~HelloTriangle()
 
 bool HelloTriangle::prepare(const vkb::ApplicationOptions &options)
 {
+	// Headless is not supported to keep this sample as simple as possible
 	assert(options.window != nullptr);
+	assert(options.window->get_window_mode() != vkb::Window::Mode::Headless);
 
 	init_instance(context, {VK_KHR_SURFACE_EXTENSION_NAME}, {});
 
