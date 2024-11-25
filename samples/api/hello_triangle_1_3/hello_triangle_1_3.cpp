14a06bfa1ef2531d5392750ea10a475251fc0aa9
aliasifhuawei <ali.asif@huawei.com>
2024-11-25 14:05:27 -0800

    Added topology for VkPipelineInputAssemblyStateCreateInfo

--- samples/api/hello_triangle_1_3/hello_triangle_1_3.cpp
+++ samples/api/hello_triangle_1_3/hello_triangle_1_3.cpp
@@ -786,6 +786,7 @@ void HelloTriangleV13::init_pipeline()
 	// Specify we will use triangle lists to draw geometry.
 	VkPipelineInputAssemblyStateCreateInfo input_assembly{
 	    .sType                  = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+	    .topology               = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
 	    .primitiveRestartEnable = VK_FALSE};
 
 	// Specify rasterization state.
