4a2813533f30308e3056785c7395a9480c45554a
aliasifhuawei <ali.asif@huawei.com>
2024-11-27 16:53:35 -0800

    let default initialize component

--- samples/api/hello_triangle_1_3/hello_triangle_1_3.cpp
+++ samples/api/hello_triangle_1_3/hello_triangle_1_3.cpp
@@ -655,11 +655,6 @@ void HelloTriangleV13::init_swapchain()
 		    .image      = swapchain_images[i],
 		    .viewType   = VK_IMAGE_VIEW_TYPE_2D,
 		    .format     = context.swapchain_dimensions.format,
-		    .components = {
-		        .r = VK_COMPONENT_SWIZZLE_R,
-		        .g = VK_COMPONENT_SWIZZLE_G,
-		        .b = VK_COMPONENT_SWIZZLE_B,
-		        .a = VK_COMPONENT_SWIZZLE_A},
 		    .subresourceRange = {.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT, .baseMipLevel = 0, .levelCount = 1, .baseArrayLayer = 0, .layerCount = 1}};
 
 		VkImageView image_view;
