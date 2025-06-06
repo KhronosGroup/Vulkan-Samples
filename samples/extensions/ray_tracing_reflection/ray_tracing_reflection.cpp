3ec076657ca17d649a5eae7cd6c979a8abbbe88a
Sascha Willems <webmaster@saschawillems.de>
2025-06-06 19:38:05 +0200

    Copyright fixes

--- samples/extensions/ray_tracing_reflection/ray_tracing_reflection.cpp
+++ samples/extensions/ray_tracing_reflection/ray_tracing_reflection.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2021-2025, NVIDIA CORPORATION.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * SPDX-FileCopyrightText: Copyright (c) 2014-2024 NVIDIA CORPORATION
- * SPDX-License-Identifier: Apache-2.0
  */
 
 /*
