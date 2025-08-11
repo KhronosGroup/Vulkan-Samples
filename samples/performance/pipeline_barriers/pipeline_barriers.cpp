ebf8f8376a59c2e8593dcaab1d3bef0b365d439d
SRSaunders <82544213+SRSaunders@users.noreply.github.com>
2025-08-11 00:21:21 -0400

    Harmonize layer setting logic for MoltenVK config across affected samples

--- samples/performance/pipeline_barriers/pipeline_barriers.cpp
+++ samples/performance/pipeline_barriers/pipeline_barriers.cpp
@@ -38,6 +38,23 @@ PipelineBarriers::PipelineBarriers()
 	config.insert<vkb::IntSetting>(0, reinterpret_cast<int &>(dependency_type), DependencyType::BOTTOM_TO_TOP);
 	config.insert<vkb::IntSetting>(1, reinterpret_cast<int &>(dependency_type), DependencyType::FRAG_TO_VERT);
 	config.insert<vkb::IntSetting>(2, reinterpret_cast<int &>(dependency_type), DependencyType::FRAG_TO_FRAG);
+
+#if defined(PLATFORM__MACOS) && TARGET_OS_IOS && TARGET_OS_SIMULATOR
+	// On iOS Simulator use layer setting to disable MoltenVK's Metal argument buffers - otherwise blank display
+	add_instance_extension(VK_EXT_LAYER_SETTINGS_EXTENSION_NAME, /*optional*/ true);
+
+	VkLayerSettingEXT layerSetting;
+	layerSetting.pLayerName   = "MoltenVK";
+	layerSetting.pSettingName = "MVK_CONFIG_USE_METAL_ARGUMENT_BUFFERS";
+	layerSetting.type         = VK_LAYER_SETTING_TYPE_INT32_EXT;
+	layerSetting.valueCount   = 1;
+
+	// Make this static so layer setting reference remains valid after leaving constructor scope
+	static const int32_t useMetalArgumentBuffers = 0;
+	layerSetting.pValues                         = &useMetalArgumentBuffers;
+
+	add_layer_setting(layerSetting);
+#endif
 }
 
 bool PipelineBarriers::prepare(const vkb::ApplicationOptions &options)
