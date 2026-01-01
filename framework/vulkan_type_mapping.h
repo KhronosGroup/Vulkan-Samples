11ee2fba60e93203d81d50c1328e7f25184c31ba
swinston <steve@holochip.com>
2025-12-31 20:52:25 -0800

    Update copyright years to 2026
    Updates copyright year ranges across multiple files to include 2026. Also fixes a typo in one header (202 -> 2026) and aligns code formatting for improved readability.
    
    Happy New Year!

--- framework/vulkan_type_mapping.h
+++ framework/vulkan_type_mapping.h
@@ -1,5 +1,5 @@
-/* Copyright (c) 2025, Arm Limited and Contributors
- * Copyright (c) 2024-2025, NVIDIA CORPORATION. All rights reserved.
+/* Copyright (c) 2026, Arm Limited and Contributors
+ * Copyright (c) 2024-2026, NVIDIA CORPORATION. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
