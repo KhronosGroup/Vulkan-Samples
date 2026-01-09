2f4d3dcddfcf8ad3c99094ede89de46601921701
swinston <steve@holochip.com>
2026-01-09 01:06:45 -0800

    Update copyright year to 2026 in modified files
    - Update copyright end year from 2025 to 2026 across framework and sample files
    - Affected files include swapchain_recreation sample, command pool base, vulkan resource, HPP queue, and vulkan sample headers

--- framework/core/hpp_queue.h
+++ framework/core/hpp_queue.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021-2025, NVIDIA CORPORATION. All rights reserved.
+/* Copyright (c) 2021-2026, NVIDIA CORPORATION. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
