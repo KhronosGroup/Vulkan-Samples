3db5acc7fb8cb95aedf8466d5c7ef8fec68c778a
SRSaunders <82544213+SRSaunders@users.noreply.github.com>
2026-03-01 00:51:42 -0500

    Fix layout_transitions, pipeline_barriers, & subpasses to use layer_settings framework (iOS Simulator)

--- samples/performance/pipeline_barriers/pipeline_barriers.cpp
+++ samples/performance/pipeline_barriers/pipeline_barriers.cpp
@@ -38,21 +38,6 @@ PipelineBarriers::PipelineBarriers()
 	config.insert<vkb::IntSetting>(0, reinterpret_cast<int &>(dependency_type), DependencyType::BOTTOM_TO_TOP);
 	config.insert<vkb::IntSetting>(1, reinterpret_cast<int &>(dependency_type), DependencyType::FRAG_TO_VERT);
 	config.insert<vkb::IntSetting>(2, reinterpret_cast<int &>(dependency_type), DependencyType::FRAG_TO_FRAG);
-
-#if defined(PLATFORM__MACOS) && TARGET_OS_IOS && TARGET_OS_SIMULATOR
-	// On iOS Simulator use layer setting to disable MoltenVK's Metal argument buffers - otherwise blank display
-	VkLayerSettingEXT layerSetting;
-	layerSetting.pLayerName   = "MoltenVK";
-	layerSetting.pSettingName = "MVK_CONFIG_USE_METAL_ARGUMENT_BUFFERS";
-	layerSetting.type         = VK_LAYER_SETTING_TYPE_INT32_EXT;
-	layerSetting.valueCount   = 1;
-
-	// Make this static so layer setting reference remains valid after leaving constructor scope
-	static const int32_t disableMetalArgumentBuffers = 0;
-	layerSetting.pValues                             = &disableMetalArgumentBuffers;
-
-	add_layer_setting(layerSetting);
-#endif
 }
 
 bool PipelineBarriers::prepare(const vkb::ApplicationOptions &options)
@@ -132,6 +117,15 @@ void PipelineBarriers::request_instance_extensions(std::unordered_map<std::strin
 	// On iOS Simulator use layer setting to disable MoltenVK's Metal argument buffers - otherwise blank display
 	vkb::VulkanSampleC::request_instance_extensions(requested_extensions);
 	requested_extensions[VK_EXT_LAYER_SETTINGS_EXTENSION_NAME] = vkb::RequestMode::Optional;
+}
+
+void PipelineBarriers::request_layer_settings(std::vector<VkLayerSettingEXT> &requested_layer_settings) const
+{
+	// Make this static so layer setting reference remains valid after leaving the current scope
+	static const int32_t disableMetalArgumentBuffers = 0;
+
+	vkb::VulkanSampleC::request_layer_settings(requested_layer_settings);
+	requested_layer_settings.push_back({"MoltenVK", "MVK_CONFIG_USE_METAL_ARGUMENT_BUFFERS", VK_LAYER_SETTING_TYPE_INT32_EXT, 1, &disableMetalArgumentBuffers});
 }
 #endif
 
