d303b1f9bc72f8853a7d5b05f4e3c85d75a82838
SRSaunders <82544213+SRSaunders@users.noreply.github.com>
2026-06-01 11:24:14 -0400

    Set VK_LAYER_KHRONOS_validation to Optional in case layer not available (#1522)
    * Set VK_LAYER_KHRONOS_validation to Optional in case layer not available for debug builds
    
    * Ensure device is defined in sample cleanup code - avoid crashes on exit
    
    * Configure dynamic_rendering_local_read to render properly on iOS Simulator
    
    * Set gui depth format as VK_FORMAT_UNDEFINED for dynamic rendering
    
    * Update dynamic_rendering_local_read to use structure chain builder for layer setting
    
    * Make VK_LAYER_KHRONOS_validation optional for debug builds only

--- samples/performance/layout_transitions/layout_transitions.cpp
+++ samples/performance/layout_transitions/layout_transitions.cpp
@@ -79,13 +79,13 @@ bool LayoutTransitions::prepare(const vkb::ApplicationOptions &options)
 #if defined(PLATFORM__MACOS) && TARGET_OS_IOS && TARGET_OS_SIMULATOR
 void LayoutTransitions::request_instance_extensions(std::unordered_map<std::string, vkb::RequestMode> &requested_extensions) const
 {
-	// On iOS Simulator use layer setting to disable MoltenVK's Metal argument buffers - otherwise blank display
 	vkb::VulkanSampleC::request_instance_extensions(requested_extensions);
 	requested_extensions[VK_EXT_LAYER_SETTINGS_EXTENSION_NAME] = vkb::RequestMode::Optional;
 }
 
 void LayoutTransitions::request_layer_settings(std::vector<VkLayerSettingEXT> &requested_layer_settings, vkb::StructureChainBuilderC<VkInstanceCreateInfo> &scb) const
 {
+	// On iOS Simulator use layer setting to disable MoltenVK's Metal argument buffers - otherwise blank display
 	vkb::VulkanSampleC::request_layer_settings(requested_layer_settings, scb);
 	requested_layer_settings.push_back({"MoltenVK", "MVK_CONFIG_USE_METAL_ARGUMENT_BUFFERS", VK_LAYER_SETTING_TYPE_INT32_EXT, 1, &scb.add_chain_data<int32_t>(0)});
 }
